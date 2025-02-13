b247bf4ed253e18b6c3048ae3a7a4e88ea6dcb3b
Chan <abc1277046286@gmail.com>
2025-02-13 16:17:06 +0800

    fix(auth):设置为只要redis中找不到Token，就是token被作废

--- service/auth/rpc/internal/logic/verifytokenbyrpclogic.go
+++ service/auth/rpc/internal/logic/verifytokenbyrpclogic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"context"
 	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"gomall/common/jwtx"
 	"gomall/service/auth/rpc/internal/svc"
 	"gomall/service/auth/rpc/types/auth"
@@ -27,7 +28,7 @@ func NewVerifyTokenByRPCLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 
 // VerifyTokenByRPC 验证accessToken是否有效
 func (l *VerifyTokenByRPCLogic) VerifyTokenByRPC(in *auth.VerifyTokenReq) (*auth.VerifyResp, error) {
-	// 先去数据库检查该值是否存在，存在说明无效
+	// 先去数据库检查该值是否存在，不存在说明无效
 	var err error
 	if in.TokenType == "access" {
 		err = l.svcCtx.RDB.Get(l.ctx, fmt.Sprintf("accessToken:%d:%s", in.UserId, in.Token)).Err()
@@ -36,15 +37,13 @@ func (l *VerifyTokenByRPCLogic) VerifyTokenByRPC(in *auth.VerifyTokenReq) (*auth
 	}
 
 	if err != nil {
-		//if err != redis.Nil {
-		//	// redis 数据库故障，视为无缓存模式
-		//	logx.Errorf("Redis error:%s", err.Error())
-		//} else if err == redis.Nil {
-		//	// redis 未找到该值，视为无缓存模式
-		//	logx.Info("redis not found token, execute check token process later...")
-		//	err = nil
-		//}
-		logx.Info("redis is down or not found token, execute check token process later")
+		if err != redis.Nil {
+			// redis 数据库故障，视为无缓存模式，进行token的检查
+			logx.Errorf("Redis error:%s", err.Error())
+		} else if err == redis.Nil {
+			// redis 未找到该值，说明已被删除
+			return &auth.VerifyResp{Res: false, UserId: 0}, err
+		}
 	} else {
 		// redis找到token了，直接返回true
 		//logx.Info("token is valid")
